d63749d2ac3dcdbaed24d56f71e9ef47027f944c
felipedelatorre <felipede_latorre@hotmail.com>
2019-07-02 21:07:07 -0700

    change color pallet and complete modularization

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './header/header';
-import Footer from './footer/footer';
-import Counter from './counter/counter';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
 import './app.scss';
 
 
